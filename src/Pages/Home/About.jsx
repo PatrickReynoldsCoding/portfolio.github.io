37a74d672f31af0ce6d5558846fbc51ab3f8901d
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-01-23 23:35:31 +0000

    fading animations working

--- src/Pages/Home/About.jsx
+++ src/Pages/Home/About.jsx
@@ -1,35 +1,81 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
-
-const AboutMeBoxContainer = styled.div`
-  display: flex;
-`;
-
-const AboutMeBox = styled.div`
-  background-color: white;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 0px;
-  height: 60px;
-  width: 100vw;
-  background-color: #333;
-  color: #fff;
-`;
+import "./about.css";
 
 export default function About() {
+  const observerRef = useRef(null);
+
+  useEffect(() => {
+    observerRef.current = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    });
+    const hiddenElements = document.querySelectorAll(".hidden");
+    hiddenElements.forEach((el) => observerRef.current.observe(el));
+    return () => observerRef.current.disconnect();
+  }, []);
   return (
     <div>
-      <AboutMeBoxContainer>
-        <AboutMeBox>
+      <section ref={observerRef} className="hidden">
+        <h2>Yo sup my doodes</h2>
+        <p>
           Lorem ipsum dolor sit amet. Maiores exercitationem harum rerum nobis!
           In dolor ipsa blanditiis perspiciatis! Eius aliquid rem exercitationem
           nihil. Ipsa accusantium hic laborum vero. Porro tempore voluptates
           explicabo quis? Quidem iste maiores exercitationem odit? Dolorem
           accusamus et consequatur tempora? Dolore, debitis! Vero, ratione
           minus. Iusto saepe odit nam sint?
-        </AboutMeBox>
-      </AboutMeBoxContainer>
+        </p>
+      </section>
+      <section ref={observerRef} className="hidden">
+        <h2>Yo sup my doodes</h2>
+        <p>
+          Lorem ipsum dolor sit amet. Maiores exercitationem harum rerum nobis!
+          In dolor ipsa blanditiis perspiciatis! Eius aliquid rem exercitationem
+          nihil. Ipsa accusantium hic laborum vero. Porro tempore voluptates
+          explicabo quis? Quidem iste maiores exercitationem odit? Dolorem
+          accusamus et consequatur tempora? Dolore, debitis! Vero, ratione
+          minus. Iusto saepe odit nam sint?
+        </p>
+      </section>
+      <section ref={observerRef} className="hidden">
+        <h2>Yo sup my doodes</h2>
+        <p>
+          Lorem ipsum dolor sit amet. Maiores exercitationem harum rerum nobis!
+          In dolor ipsa blanditiis perspiciatis! Eius aliquid rem exercitationem
+          nihil. Ipsa accusantium hic laborum vero. Porro tempore voluptates
+          explicabo quis? Quidem iste maiores exercitationem odit? Dolorem
+          accusamus et consequatur tempora? Dolore, debitis! Vero, ratione
+          minus. Iusto saepe odit nam sint?
+        </p>
+      </section>
+      <section ref={observerRef} className="hidden">
+        <h2>Yo sup my doodes</h2>
+        <p>
+          Lorem ipsum dolor sit amet. Maiores exercitationem harum rerum nobis!
+          In dolor ipsa blanditiis perspiciatis! Eius aliquid rem exercitationem
+          nihil. Ipsa accusantium hic laborum vero. Porro tempore voluptates
+          explicabo quis? Quidem iste maiores exercitationem odit? Dolorem
+          accusamus et consequatur tempora? Dolore, debitis! Vero, ratione
+          minus. Iusto saepe odit nam sint?
+        </p>
+      </section>
+      <section ref={observerRef} className="hidden">
+        <h2>Yo sup my doodes</h2>
+        <p>
+          Lorem ipsum dolor sit amet. Maiores exercitationem harum rerum nobis!
+          In dolor ipsa blanditiis perspiciatis! Eius aliquid rem exercitationem
+          nihil. Ipsa accusantium hic laborum vero. Porro tempore voluptates
+          explicabo quis? Quidem iste maiores exercitationem odit? Dolorem
+          accusamus et consequatur tempora? Dolore, debitis! Vero, ratione
+          minus. Iusto saepe odit nam sint?
+        </p>
+      </section>
     </div>
   );
 }
