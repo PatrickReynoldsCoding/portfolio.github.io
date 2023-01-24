5c777d73a0a55797eeedf8b9b705497e9e3fff8f
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-01-24 00:38:28 +0000

    corrected spelling error in Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 // Components
 import TopToggle from "./TopToggle";
 import { HomeAnimation } from "./HomeAnimation";
-import About from "./about";
+import About from "./About";
 
 export default function Home() {
   return (
