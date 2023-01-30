ee3b1358de9ddc1b5cbb1ff0c03a28e8a6b7affe
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-01-30 12:41:41 +0000

    conditional added to home screen

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
 import styled from "styled-components";
 
 // Components
@@ -7,11 +7,12 @@ import { HomeAnimation } from "./HomeAnimation";
 import About from "./About";
 
 export default function Home() {
+  const [isAboutActive, setIsAboutActive] = useState(true);
   return (
     <div>
       <HomeAnimation className="animation-container" />
       <TopToggle />
-      <About />
+      {isAboutActive ? <About /> : <Projects />}
     </div>
   );
 }
