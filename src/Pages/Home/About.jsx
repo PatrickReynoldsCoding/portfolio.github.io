08e7e93254a657a18ff64f7d1b20808dad965c5c
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-01-30 11:22:48 +0000

    scroll snap working

--- src/Pages/Home/About.jsx
+++ src/Pages/Home/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
         if (entry.isIntersecting) {
           entry.target.classList.add("show");
           window.scrollTo({
-            top: entry.target.offsetTop,
+            top: entry.target.offsetTop - window.innerHeight / 2,
             left: 0,
             behavior: "smooth",
           });
@@ -25,66 +25,77 @@ export default function About() {
   }, []);
   return (
     <article className="about-container">
-      <section ref={observerRef} className="hidden scroll-area">
-        <h2>About</h2>
-        <p>
-          I am a dedicated developer with a passion for building apps and
-          websites. <br />
-          <br />
-          Recently completing a 16-week coding Bootcamp at Makers.com I am now
-          seeking employment opportunities to further advance my skills and
-          portfolio. I am particularly focused on expanding my knowledge of
-          design principles and exploring new and exciting areas of development.
-          <br />
-          <br />
-          Tech Skills: JavaScript, React.js, Node.js, Express, Jest, Cypress,
-          HTML, SCSS, CSS, Ruby, Sinatra, RSpec
-        </p>
-      </section>
-      <section ref={observerRef} className="hidden left scroll-area">
-        <h2>Yo sup my doodes</h2>
-        <p>
-          Lorem ipsum dolor sit amet. Maiores exercitationem harum rerum nobis!
-          In dolor ipsa blanditiis perspiciatis! Eius aliquid rem exercitationem
-          nihil. Ipsa accusantium hic laborum vero. Porro tempore voluptates
-          explicabo quis? Quidem iste maiores exercitationem odit? Dolorem
-          accusamus et consequatur tempora? Dolore, debitis! Vero, ratione
-          minus. Iusto saepe odit nam sint?
-        </p>
-      </section>
-      <section ref={observerRef} className="hidden scroll-area">
-        <h2>Yo sup my doodes</h2>
-        <p>
-          Lorem ipsum dolor sit amet. Maiores exercitationem harum rerum nobis!
-          In dolor ipsa blanditiis perspiciatis! Eius aliquid rem exercitationem
-          nihil. Ipsa accusantium hic laborum vero. Porro tempore voluptates
-          explicabo quis? Quidem iste maiores exercitationem odit? Dolorem
-          accusamus et consequatur tempora? Dolore, debitis! Vero, ratione
-          minus. Iusto saepe odit nam sint?
-        </p>
-      </section>
-      <section ref={observerRef} className="hidden left scroll-area">
-        <h2>Yo sup my doodes</h2>
-        <p>
-          Lorem ipsum dolor sit amet. Maiores exercitationem harum rerum nobis!
-          In dolor ipsa blanditiis perspiciatis! Eius aliquid rem exercitationem
-          nihil. Ipsa accusantium hic laborum vero. Porro tempore voluptates
-          explicabo quis? Quidem iste maiores exercitationem odit? Dolorem
-          accusamus et consequatur tempora? Dolore, debitis! Vero, ratione
-          minus. Iusto saepe odit nam sint?
-        </p>
-      </section>
-      <section ref={observerRef} className="hidden left scroll-area">
-        <h2>Yo sup my doodes</h2>
-        <p>
-          Lorem ipsum dolor sit amet. Maiores exercitationem harum rerum nobis!
-          In dolor ipsa blanditiis perspiciatis! Eius aliquid rem exercitationem
-          nihil. Ipsa accusantium hic laborum vero. Porro tempore voluptates
-          explicabo quis? Quidem iste maiores exercitationem odit? Dolorem
-          accusamus et consequatur tempora? Dolore, debitis! Vero, ratione
-          minus. Iusto saepe odit nam sint?
-        </p>
-      </section>
+      <div ref={observerRef} className="scroll-area">
+        <section className="hidden">
+          <h2>About</h2>
+          <p>
+            I am a dedicated developer with a passion for building apps and
+            websites. <br />
+            <br />
+            Recently completing a 16-week coding Bootcamp at Makers.com I am now
+            seeking employment opportunities to further advance my skills and
+            portfolio. I am particularly focused on expanding my knowledge of
+            design principles and exploring new and exciting areas of
+            development.
+            <br />
+            <br />
+            Tech Skills: JavaScript, React.js, Node.js, Express, Jest, Cypress,
+            HTML, SCSS, CSS, Ruby, Sinatra, RSpec
+          </p>
+        </section>
+      </div>
+      <div ref={observerRef} className="scroll-area">
+        <section className="hidden left">
+          <h2>Yo sup my doodes</h2>
+          <p>
+            Lorem ipsum dolor sit amet. Maiores exercitationem harum rerum
+            nobis! In dolor ipsa blanditiis perspiciatis! Eius aliquid rem
+            exercitationem nihil. Ipsa accusantium hic laborum vero. Porro
+            tempore voluptates explicabo quis? Quidem iste maiores
+            exercitationem odit? Dolorem accusamus et consequatur tempora?
+            Dolore, debitis! Vero, ratione minus. Iusto saepe odit nam sint?
+          </p>
+        </section>
+      </div>
+      <div ref={observerRef} className="scroll-area">
+        <section className="hidden">
+          <h2>Yo sup my doodes</h2>
+          <p>
+            Lorem ipsum dolor sit amet. Maiores exercitationem harum rerum
+            nobis! In dolor ipsa blanditiis perspiciatis! Eius aliquid rem
+            exercitationem nihil. Ipsa accusantium hic laborum vero. Porro
+            tempore voluptates explicabo quis? Quidem iste maiores
+            exercitationem odit? Dolorem accusamus et consequatur tempora?
+            Dolore, debitis! Vero, ratione minus. Iusto saepe odit nam sint?
+          </p>
+        </section>
+      </div>
+      <div ref={observerRef} className="scroll-area">
+        <section className="hidden left">
+          <h2>Yo sup my doodes</h2>
+          <p>
+            Lorem ipsum dolor sit amet. Maiores exercitationem harum rerum
+            nobis! In dolor ipsa blanditiis perspiciatis! Eius aliquid rem
+            exercitationem nihil. Ipsa accusantium hic laborum vero. Porro
+            tempore voluptates explicabo quis? Quidem iste maiores
+            exercitationem odit? Dolorem accusamus et consequatur tempora?
+            Dolore, debitis! Vero, ratione minus. Iusto saepe odit nam sint?
+          </p>
+        </section>
+      </div>
+      <div ref={observerRef} className="scroll-area">
+        <section className="hidden left">
+          <h2>Yo sup my doodes</h2>
+          <p>
+            Lorem ipsum dolor sit amet. Maiores exercitationem harum rerum
+            nobis! In dolor ipsa blanditiis perspiciatis! Eius aliquid rem
+            exercitationem nihil. Ipsa accusantium hic laborum vero. Porro
+            tempore voluptates explicabo quis? Quidem iste maiores
+            exercitationem odit? Dolorem accusamus et consequatur tempora?
+            Dolore, debitis! Vero, ratione minus. Iusto saepe odit nam sint?
+          </p>
+        </section>
+      </div>
     </article>
   );
 }
