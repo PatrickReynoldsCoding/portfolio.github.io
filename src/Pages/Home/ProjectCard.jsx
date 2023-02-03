45989f79ef00c3e6a3e310aa16ac7031ffd9fd31
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-02-03 14:35:28 +0000

    project cards working

--- src/Pages/Home/ProjectCard.jsx
+++ src/Pages/Home/ProjectCard.jsx
@@ -4,7 +4,10 @@ import "./projects.css";
 export default function ProjectCard(props) {
   return (
     <div className="project-card">
-      <div className="project-card-text">Project Text</div>
+      <div className="project-card-img"></div>
+      <div className="project-card-text">
+        <h3>Project Text</h3>
+      </div>
     </div>
   );
 }
