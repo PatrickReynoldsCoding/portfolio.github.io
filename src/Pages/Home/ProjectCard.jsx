75b9cd3c5c6ad6808917f2901bc73137e01e4a39
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-02-03 15:02:33 +0000

    project cards complete. need to fill out projects object in ProjectCardContainer

--- src/Pages/Home/ProjectCard.jsx
+++ src/Pages/Home/ProjectCard.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import "./projects.css";
 
 export default function ProjectCard(props) {
-  const gif = "../../public/project_card_images/PokemonMatch.gif";
   return (
     <div className="project-card">
-      <img className="project-card-img" src={gif} />
+      <img className="project-card-img" src={props.project.gif} />
       <div className="project-card-text">
-        <h3>Project Text</h3>
+        <h3>{props.project.key}</h3>
       </div>
     </div>
   );
