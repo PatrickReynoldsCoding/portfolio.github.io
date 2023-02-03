671e9b0d278a9fcb620ea7508ca4fd4736ce5782
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-02-03 13:33:17 +0000

    fixing the div to img on project page

--- src/Pages/Home/ProjectCard.jsx
+++ src/Pages/Home/ProjectCard.jsx
@@ -4,7 +4,7 @@ import "./projects.css";
 export default function ProjectCard(props) {
   return (
     <img
-      className="project-card-img"
+      className="project-card"
       src="./project_card_images/PokemonMatch.gif"
       alt="test 1"
     />
