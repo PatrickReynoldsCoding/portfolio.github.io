f37133a36efc91d90746d9508c06cb0bfd0e4d18
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-02-04 13:00:44 +0000

    gif testing - changing relative path to absolute

--- src/Pages/Home/ProjectCardContainer.jsx
+++ src/Pages/Home/ProjectCardContainer.jsx
@@ -30,7 +30,7 @@ export default function ProjectCardContainer() {
         github: "https://github.com/PatrickReynoldsCoding/pokemon-match",
       },
       images: {
-        gif: "../../public/project_card_images/PokemonMatch(low).gif",
+        gif: "https://patrickreynolds.dev/public/project_card_images/PokemonMatch(low).gif",
         projectImage: "",
       },
     },
