526f4c05ed151feffd984cce562676edd7857d7f
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-02-04 13:04:57 +0000

    gif testing - adding type="image/gif" to html

--- src/Pages/Home/ProjectCard.jsx
+++ src/Pages/Home/ProjectCard.jsx
@@ -5,7 +5,11 @@ import "./projects.css";
 export default function ProjectCard(props) {
   return (
     <div className="project-card">
-      <img className="project-card-img" src={props.project.images.gif} />
+      <img
+        className="project-card-img"
+        src={props.project.images.gif}
+        type="image/gif"
+      />
       <div className="project-card-text">
         <h3>{props.project.key}</h3>
       </div>
