9b07f7caa50aeb664c8029a3066a7af02b0d97b2
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-02-07 13:00:59 +0000

    updated project object

--- src/Pages/Home/ProjectCardContainer.jsx
+++ src/Pages/Home/ProjectCardContainer.jsx
@@ -10,14 +10,17 @@ export default function ProjectCardContainer() {
       key: "Pokemon Match",
       subtitle:
         "A Pokemon memory match game. Match all 6 pokemon to set 3 stars. But, Watch out for Team Rocket",
-      details: "",
-      links: {
-        website: {
-          url: "https://vercel.com/patrickreynoldscoding/pokemon-match",
-          deployed: true,
+      details: {
+        paragraphs: [],
+        links: {
+          website: {
+            url: "https://vercel.com/patrickreynoldscoding/pokemon-match",
+            deployed: true,
+          },
+          github: "https://github.com/PatrickReynoldsCoding/pokemon-match",
         },
-        github: "https://github.com/PatrickReynoldsCoding/pokemon-match",
       },
+
       media: {
         gif: "./project_card_images/project-gifs/PokemonMatch-high.gif",
         projectImage: "",
@@ -27,13 +30,15 @@ export default function ProjectCardContainer() {
       key: "SweepStakes 2022",
       subtitle:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, consequatur!",
-      details: "",
-      links: {
-        website: {
-          url: "https://chowie.uk/",
-          deployed: false,
+      details: {
+        paragraphs: [],
+        links: {
+          website: {
+            url: "https://chowie.uk/",
+            deployed: false,
+          },
+          github: "https://github.com/chowieuk/sweepstakes-app",
         },
-        github: "https://github.com/chowieuk/sweepstakes-app",
       },
       media: {
         gif: "./project_card_images/project-gifs/sweepstakes-high.gif",
@@ -52,10 +57,10 @@ export default function ProjectCardContainer() {
         ],
         links: {
           website: {
-            url: "https://vercel.com/patrickreynoldscoding/pokemon-match",
-            deployed: true,
+            url: null,
+            deployed: false,
           },
-          github: "https://github.com/PatrickReynoldsCoding/pokemon-match",
+          github: "https://github.com/PatrickReynoldsCoding/pair-up-live/",
         },
       },
       media: {
@@ -75,16 +80,22 @@ export default function ProjectCardContainer() {
       },
     },
     {
-      key: "The Facebook",
-      subtitle:
-        "A Pokemon memory match game. Match all 6 pokemon to set 3 stars. But, Watch out for Team Rocket",
-      details: "",
-      links: {
-        website: {
-          url: "https://vercel.com/patrickreynoldscoding/pokemon-match",
-          deployed: true,
+      key: "Acebook",
+      subtitle: "A Facebook clone focusing on backend technologies",
+      details: {
+        paragraphs: [
+          "Welcome to Acebook! We took everything that people liked about Facebook and made our own version without invading your privacy...or are we mining your data? You'll never know, without diving into the source code above.",
+          "This Makers Academy Bootcamp project, Acebook, is a simplified version of a social media platform built with a focus on its backend technology stack.",
+          "The project utilized Express, a popular web framework for Node.js, to handle server-side routing and operations. Nodemon was used to automatically reload the server during development, while Handlebars was utilized to render view templates. Mongoose was employed to model data objects in MongoDB, ensuring efficient and organized data management. ESLint was utilized for linting and Jest was employed for testing to ensure code quality. End-to-end testing was performed using Cypress, and the project was integrated with Github for Continuous Integration (CI).",
+          "This one-week project focused on mastery of key backend technologies and best practices.",
+        ],
+        links: {
+          website: {
+            url: "",
+            deployed: false,
+          },
+          github: "https://github.com/PatrickReynoldsCoding/the-fakebook",
         },
-        github: "https://github.com/PatrickReynoldsCoding/pokemon-match",
       },
       media: {
         gif: "./project_card_images/project-gifs/PokemonMatch-high.gif",
