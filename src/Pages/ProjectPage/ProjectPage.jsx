b557a0010eb9482dff8ca72e3210beb9746f9719
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-02-09 12:05:58 +0000

    created data directory

--- src/Pages/ProjectPage/ProjectPage.jsx
+++ src/Pages/ProjectPage/ProjectPage.jsx
@@ -5,20 +5,21 @@ export default function ProjectPage() {
   const location = useLocation();
   const { project } = location.state;
 
-  const setMainImages = (imgs) => {
-    // Get the expanded image
-    let expandImg = document.getElementById("expandedImg");
-    // Use the same src in the expanded image as the image being clicked on from the grid
-    expandImg.src = imgs.src;
-    // Use the value of the alt attribute of the clickable image as text inside the expanded image
-    imgText.innerHTML = imgs.alt;
-    // Show the container element (hidden with CSS)
-    expandImg.parentElement.style.display = "block";
-  };
+  // const setMainImages = (imgs) => {
+  //   // Get the expanded image
+  //   let expandImg = document.getElementById("expandedImg");
+  //   // Use the same src in the expanded image as the image being clicked on from the grid
+  //   expandImg.src = imgs.src;
+  //   // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  //   imgText.innerHTML = imgs.alt;
+  //   // Show the container element (hidden with CSS)
+  //   expandImg.parentElement.style.display = "block";
+  // };
 
   return (
     <div className="project-page-wrapper">
-      <div className="project page image">
+      <h1>{project.title}</h1>
+      {/* <div className="project page image">
         <div className="project-page-main-image">
           <img id="expandedImg" />
         </div>
@@ -37,7 +38,7 @@ export default function ProjectPage() {
       <div className="project-page-details">
         <div className="project-page-paragraphs"></div>
         <div className="project-page-link"></div>
-      </div>
+      </div> */}
     </div>
   );
 }
